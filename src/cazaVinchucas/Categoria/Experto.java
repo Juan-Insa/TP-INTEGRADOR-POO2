de7ea6b30dcdbc93e3b917cb563d6095d7cbfd78
ivanapr <vanndere@gmail.com>
2022-06-10 21:10:16 -0300

    Fix recategorizacion ahora considera la fecha de muestras y opiniones

--- src/cazaVinchucas/Categoria/Experto.java
+++ src/cazaVinchucas/Categoria/Experto.java
@@ -1,6 +1,12 @@
 package cazaVinchucas.Categoria;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import cazaVinchucas.Opinion;
 import cazaVinchucas.Sistema;
+import cazaVinchucas.muestras.Muestra;
 
 /**
  * Clase encargada de representar el comportamiento particular de una 
@@ -24,9 +30,12 @@ public class Experto extends Categoria {
 	 * @param usuario es el usuario actual
 	 */
 	public void recategorizar(Sistema sistema, Usuario usuario) {
-		//B�sico: para aquellas personas que reci�n comienzan a participar. Un participante nuevo posee nivel b�sico.
-		//Experto: son personas que durante los �ltimos 30 d�as desde la fecha actual han realizado m�s de 10 env�os y m�s de 20 revisiones.
-		if (!((sistema.getMuestrasDe(usuario).size() > 10) && (sistema.getOpinionesDe(usuario).size() > 20))) {
+		LocalDate ultimos30Dias = LocalDate.now().minusDays(30);
+		List<Muestra> muestrasConsideradas = sistema.getMuestrasDe(usuario).stream().filter(m -> (m.getFecha().isAfter(ultimos30Dias))).collect(Collectors.toList());
+		List<Opinion> opinionesConsideradas = sistema.getOpinionesDe(usuario).stream().filter(o -> (o.getFecha().isAfter(ultimos30Dias))).collect(Collectors.toList());
+		//Basico: para aquellas personas que recien comienzan a participar. Un participante nuevo posee nivel basico.
+		//Experto: son personas que durante los utimos 30 dias desde la fecha actual han realizado mas de 10 envios y mas de 20 revisiones.
+		if (!((muestrasConsideradas.size() > 10) && (opinionesConsideradas.size() > 20))) {
 			usuario.setCategoria(new Basico());
 		}
 	}
