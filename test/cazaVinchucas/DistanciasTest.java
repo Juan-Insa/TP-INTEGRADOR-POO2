570a3b6709caebda08f3ee9238d55667fb20798d
Fernando Romero <fer.rom.mu@gmail.com>
2022-06-10 22:25:04 -0300

    Se actualizo test de distancias.

--- test/cazaVinchucas/DistanciasTest.java
+++ test/cazaVinchucas/DistanciasTest.java
@@ -1,20 +1,33 @@
 package cazaVinchucas;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DistanciasTest {
 
-	Ubicacion unq, constitucionCapital, madridEspaña, cairoEgipto;
+	Ubicacion unq, constitucionCapital, madridEspaña, cairoEgipto; //SUT
 	
 	@BeforeEach
 	void setUp() throws Exception {
-		unq = new Ubicacion(-34.707023d, -58.279492d);
-		constitucionCapital = new Ubicacion(-34.628066d,-58.379954d);
-		madridEspaña = new Ubicacion(40.415188d,-3.7070433d);
-		cairoEgipto = new Ubicacion(30.047279d,31.224818);
+		
+		unq = mock(Ubicacion.class); 
+		when(unq.getLatitud()).thenReturn(-34.707023d);
+		when(unq.getLongitud()).thenReturn(-58.279492d);
+		
+		constitucionCapital = mock(Ubicacion.class); 
+		when(constitucionCapital.getLatitud()).thenReturn(-34.628066d);
+		when(constitucionCapital.getLongitud()).thenReturn(-58.379954d);
+		
+		madridEspaña = mock(Ubicacion.class); 
+		when(madridEspaña.getLatitud()).thenReturn(40.415188d);
+		when(madridEspaña.getLongitud()).thenReturn(-3.7070433d);
+		
+		cairoEgipto = mock(Ubicacion.class); 
+		when(cairoEgipto.getLatitud()).thenReturn(30.047279d);
+		when(cairoEgipto.getLongitud()).thenReturn(31.224818);
 	}
 
 	@Test
@@ -29,7 +42,7 @@ class DistanciasTest {
 	}
 
 	@Test
-	void testSetRadios() {
+	void testErrorsSetRadios() {
 		//SetUp
 		Exception e = null;
 		Exception e2 = null;
@@ -59,5 +72,48 @@ class DistanciasTest {
 		
 		//Teardown
 		CalculadoraDistancias.setRadioEcuatorialTierra();
+	}
+	
+	@Test
+	void setRadioPolarTest(){
+		//Excersice
+		CalculadoraDistancias.setRadioPolarTierra();
+		
+		//Verify
+		//Se testea que los calculos de distancia den con los valores correspondientes a usar
+		//el radio polar en vez del ecuatorial.
+		assertEquals(CalculadoraDistancias.distanciaUbicaciones(unq, madridEspaña), 10058.87d, .5d);
+		assertEquals(CalculadoraDistancias.distanciaUbicaciones(madridEspaña, unq), 10058.87d, .5d);
+		assertEquals(CalculadoraDistancias.distanciaUbicaciones(unq, constitucionCapital), 12.64d, .5d);
+		assertEquals(CalculadoraDistancias.distanciaUbicaciones(unq, cairoEgipto), 11821.5d, .5d);
+		assertEquals(CalculadoraDistancias.distanciaUbicaciones(madridEspaña, cairoEgipto), 3353.4d, .5d);
+		
+		//Teardown
+		CalculadoraDistancias.setRadioEcuatorialTierra();
+	}
+	
+	@Test
+	void radiosSuperpuestosTest() {
+		//Para este test se trazaran 3 circulos.
+		//El primero utilizará de epicentro la UNQ, el segundo capital federal y el tercero madrid.
+		//El de la Unq tendra 10000 km de radio
+		//El de capital tendrá 5km de radio
+		//El de madrid tendrá 48km de radio
+		//Como la distancia de la unq a madrid es de aprox 10047Km, el radio de madrid tocará el radio
+		//de la unq, pero no el de capital. El de capital tambien tocará a la unqui. Finalmente la unqui
+		//los toca a todos.
+		assertTrue(CalculadoraDistancias.estanRadiosSuperpuestos(10000d , unq, 48d, madridEspaña));
+		assertTrue(CalculadoraDistancias.estanRadiosSuperpuestos(48d, madridEspaña, 10000d , unq));
+		assertTrue(CalculadoraDistancias.estanRadiosSuperpuestos(10000d , unq, 5d, constitucionCapital));
+		assertFalse(CalculadoraDistancias.estanRadiosSuperpuestos(48d , madridEspaña, 5d, constitucionCapital));
+	}
+	
+	@Test
+	void estanDentroDelRadio() {
+		//Utilizando los mismos casos que el anterior test.
+		//Veremos que el circulo formado con epicentro en Quilmes y de radio de 10000km
+		//debe contener dentro a la capital, pero no a madrid.
+		assertFalse(CalculadoraDistancias.estanDentroDelRadio(10000d , unq, madridEspaña));
+		assertTrue(CalculadoraDistancias.estanDentroDelRadio(10000d , unq, constitucionCapital));
 	}
 }
